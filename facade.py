5508942f57839b165f3b42af7c0ebe6c7bf82ca1
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2022-11-13 23:14:24 -0500

    facade added to simplify program. formatData.py to be implemented to handle data results.

---
+++ facade.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from truliaBot import TruliaBot
+from realtorDotComBot import RealtorDotComBot
+from searchDetails import SearchDetails
+
+
+class Facade:
+    def __init__(self):
+        self.search_details = SearchDetails()
+        self.city = None
+        self.minimum_price = None
+        self.maximum_price = None
+        self.property_type = None
+
+    def get_search_information(self):
+        self.search_details = SearchDetails()
+        self.city = self.search_details.set_city()
+        self.minimum_price = self.search_details.set_min_price()
+        self.maximum_price = self.search_details.set_max_price()
+        self.property_type = self.search_details.set_property_type()
+
+    def comfirm_user_input(self):
+        details_confirmation = False
+        while not details_confirmation:
+            print("Please enter the following details to search for properties: ")
+
+            confirmation = input(f'\nPlease confirm the details of your search:\n'
+                                 f'City: {self.city}\n'
+                                 f'Minimum price: {self.minimum_price}\n'
+                                 f'Maximum price: {self.maximum_price}\n'
+                                 f'Property type: {self.property_type}\n\n'
+                                 f'To confirm and search press \'Y\', to re-enter the search information '
+                                 f'press \'N\': ').lower()
+
+            if confirmation != 'y':
+                print('\nPlease enter the search details again.\n')
+
+            else:
+                details_confirmation = True
+                break
+
+    def get_realtor_listings(self):
+        driver = webdriver.Firefox()
+        realtor_dot_com_bot = RealtorDotComBot(driver, self.city, self.minimum_price, self.maximum_price,
+                                               self.property_type)
+        realtor_dot_com_bot.open_webpage()
+        realtor_dot_com_bot.get_listings()
+        driver.close()
+
+    def get_trulia_listings(self):
+        driver = webdriver.Firefox()
+        trulia_bot = TruliaBot(driver, self.city, self.minimum_price, self.maximum_price,
+                               self.property_type)
+        trulia_bot.open_webpage()
+        trulia_bot.get_listings()
+        driver.close()
+
+    def run_realtor_bot(self):
+        self.get_search_information()
+        self.comfirm_user_input()
+        self.get_realtor_listings()
+
+    def run_trulia_bot(self):
+        self.get_search_information()
+        self.comfirm_user_input()
+        self.get_trulia_listings()
--- main.py
+++ main.py
@@ -1,39 +1,6 @@
-from selenium import webdriver
-from truliaBot import TruliaBot
-from realtorDotComBot import RealtorDotComBot
-from searchDetails import SearchDetails
+
+from facade import Facade
 
 if __name__ == '__main__':
-    search_details = SearchDetails()
-    city = search_details.set_city()
-    minimum_price = search_details.set_min_price()
-    maximum_price = search_details.set_max_price()
-    property_type = search_details.set_property_type()
-
-    details_confirmation = False
-    while not details_confirmation:
-        print("Please enter the following details to search for properties: ")
-
-        confirmation = input(f'\nPlease confirm the details of your search:\n'
-                             f'City: {city}\n'
-                             f'Minimum price: {minimum_price}\n'
-                             f'Maximum price: {maximum_price}\n'
-                             f'Property type: {property_type}\n\n'
-                             f'To confirm search details enter \'Y\', to re-enter the search information '
-                             f'enter \'N\': ').lower()
-
-        if confirmation != 'y':
-            print('\nPlease enter the search details again.\n')
-
-        else:
-            details_confirmation = True
-            break
-
-    driver = webdriver.Firefox()
-    realtor_dot_com_bot = RealtorDotComBot(driver, city, minimum_price, maximum_price, property_type)
-    realtor_dot_com_bot.open_webpage()
-    realtor_dot_com_bot.get_listings()
-
-    # diver2 = webdriver.Firefox()
-    # trulia_bot = TruliaBot(diver2, city, minimum_price, maximum_price, property_type)
-    # trulia_bot.open_webpage()
+    realtor_listings = Facade()
+    realtor_listings.run_realtor_bot()
